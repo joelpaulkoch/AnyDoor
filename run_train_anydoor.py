e1610b1df04efd47256591ef661b096799ef7bb1
Joel Koch <joel@joelkoch.dev>
2024-02-26 11:12:27 +0100

    Update training script

--- run_train_anydoor.py
+++ run_train_anydoor.py
@@ -13,7 +13,7 @@ from datasets.vitonhd import VitonHDDataset
 from datasets.fashiontryon import FashionTryonDataset
 from datasets.lvis import LvisDataset
 from cldm.logger import ImageLogger
-from cldm.model import create_model, load_state_dict
+from cldm.model import create_model, load_state_dicti, get_state_dict
 from torch.utils.data import ConcatDataset
 from cldm.hack import disable_verbosity, enable_sliced_attention
 from omegaconf import OmegaConf
@@ -24,13 +24,13 @@ if save_memory:
     enable_sliced_attention()
 
 # Configs
-resume_path = 'path/to/weight'
+resume_path = '/content/AnyDoor_models/general_v0.1.safetensors'
 batch_size = 16
 logger_freq = 1000
 learning_rate = 1e-5
-sd_locked = False
+sd_locked = True
 only_mid_control = False
-n_gpus = 2
+n_gpus = 1
 accumulate_grad_batches=1
 
 # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
@@ -42,23 +42,23 @@ model.only_mid_control = only_mid_control
 
 # Datasets
 DConf = OmegaConf.load('./configs/datasets.yaml')
-dataset1 = YoutubeVOSDataset(**DConf.Train.YoutubeVOS)  
-dataset2 =  SaliencyDataset(**DConf.Train.Saliency) 
-dataset3 = VIPSegDataset(**DConf.Train.VIPSeg) 
-dataset4 = YoutubeVISDataset(**DConf.Train.YoutubeVIS) 
-dataset5 = MVImageNetDataset(**DConf.Train.MVImageNet)
-dataset6 = SAMDataset(**DConf.Train.SAM)
-dataset7 = UVODataset(**DConf.Train.UVO.train)
+# dataset1 = YoutubeVOSDataset(**DConf.Train.YoutubeVOS)  
+# dataset2 =  SaliencyDataset(**DConf.Train.Saliency) 
+# dataset3 = VIPSegDataset(**DConf.Train.VIPSeg) 
+# dataset4 = YoutubeVISDataset(**DConf.Train.YoutubeVIS) 
+# dataset5 = MVImageNetDataset(**DConf.Train.MVImageNet)
+# dataset6 = SAMDataset(**DConf.Train.SAM)
+# dataset7 = UVODataset(**DConf.Train.UVO.train)
 dataset8 = VitonHDDataset(**DConf.Train.VitonHD)
-dataset9 = UVOValDataset(**DConf.Train.UVO.val)
-dataset10 = MoseDataset(**DConf.Train.Mose)
-dataset11 = FashionTryonDataset(**DConf.Train.FashionTryon)
-dataset12 = LvisDataset(**DConf.Train.Lvis)
+# dataset9 = UVOValDataset(**DConf.Train.UVO.val)
+# dataset10 = MoseDataset(**DConf.Train.Mose)
+# dataset11 = FashionTryonDataset(**DConf.Train.FashionTryon)
+# dataset12 = LvisDataset(**DConf.Train.Lvis)
 
-image_data = [dataset2, dataset6, dataset12]
-video_data = [dataset1, dataset3, dataset4, dataset7, dataset9, dataset10 ]
-tryon_data = [dataset8, dataset11]
-threed_data = [dataset5]
+image_data = [] # [dataset2, dataset6, dataset12]
+video_data = [] # [dataset1, dataset3, dataset4, dataset7, dataset9, dataset10 ]
+tryon_data = [dataset8] #, dataset11]
+threed_data = [] # [dataset5]
 
 # The ratio of each dataset is adjusted by setting the __len__ 
 dataset = ConcatDataset( image_data + video_data + tryon_data +  threed_data + video_data + tryon_data +  threed_data  )
@@ -68,3 +68,10 @@ trainer = pl.Trainer(gpus=n_gpus, strategy="ddp", precision=16, accelerator="gpu
 
 # Train!
 trainer.fit(model, dataloader)
+
+state = get_state_dict(model)
+
+import torch
+from safetensors.torch import save_file
+
+save_file(state, "/content/finetuned.safetensors")
